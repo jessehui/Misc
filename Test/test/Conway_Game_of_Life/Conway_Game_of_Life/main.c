a8546c73d5645497f0752dace698058a1bc38649
Jesse <scalpel.etf@gmail.com>
2016-02-15 21:36:04 -0600

    close all the apps on Mac

---
+++ Test/test/Conway_Game_of_Life/Conway_Game_of_Life/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Conway_Game_of_Life
+//
+//  Created by Jesse. on 16/2/15.
+//  Copyright (c) 2016年 Jesse. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
